f549440a83da134988e5567edb4abdebf156662e
soham-goswami <goswamisoham7202@gmail.com>
2025-04-11 17:29:03 -0400

    added content in main.js and main.html and implemented it

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -4,3 +4,24 @@
 # urlpatterns = [
 #     path('', views.home, name='home'),
 # ]
+
+from django.urls import path
+from . import views
+from .views import (
+    post_list_and_create,
+)
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('main/', views.main_view, name='main-board'),  # <-- this path matches /main
+]
+
+urlpatterns = [
+    path('', post_list_and_create, name='main-board'),
+]
+
+
+# urlpatterns = [
+#     path('', views.home, name='home'),
+# ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -2,3 +2,17 @@
 
 # def home(request):
 #     return HttpResponse("Hello, this is the home page!")
+from django.shortcuts import render
+from .models import Post
+
+from django.http import HttpResponse
+
+def main_view(request):
+    return HttpResponse("Welcome to the main board!")
+
+# def home(request):
+#     return render(request, 'home.html')
+
+def post_list_and_create(request):
+    qs = Post.objects.all()
+    return render(request, 'posts/main.html', {'qs':qs})
