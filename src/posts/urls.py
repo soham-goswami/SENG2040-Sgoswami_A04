b426da4b7f0695b1eb5ead898d25b842c1b25ed1
soham-goswami <goswamisoham7202@gmail.com>
2025-04-11 18:50:22 -0400

    Did my first ajax call

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -9,6 +9,8 @@ from django.urls import path
 from . import views
 from .views import (
     post_list_and_create,
+
+    hello_world_view
 )
 
 app_name = 'posts'
@@ -19,6 +21,8 @@ urlpatterns = [
 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
+
+    path('hello-world/', hello_world_view, name='hello-world'),
 ]
 
 
--- src/posts/views.py
+++ src/posts/views.py
@@ -4,7 +4,7 @@
 #     return HttpResponse("Hello, this is the home page!")
 from django.shortcuts import render
 from .models import Post
-
+from django.http import JsonResponse
 from django.http import HttpResponse
 
 def main_view(request):
@@ -16,3 +16,6 @@ def main_view(request):
 def post_list_and_create(request):
     qs = Post.objects.all()
     return render(request, 'posts/main.html', {'qs':qs})
+
+def hello_world_view(request):
+    return JsonResponse({'text': 'hello world x2'})
