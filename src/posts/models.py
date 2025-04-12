9a9a7c18bb84e8db84c14d4b9301b5b69c071858
soham-goswami <goswamisoham7202@gmail.com>
2025-04-11 21:25:24 -0400

    liked button functionality added with Ajax

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from profiles.models import Profile
-# Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -9,7 +8,11 @@ class Post(models.Model):
     liked = models.ManyToManyField(User, blank=True)
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)  # Use auto_now_add for creation date
 
-def __str__(self):
-    return str(self.title)
+    def __str__(self):
+        return str(self.title)
+
+    @property
+    def like_count(self):
+        return self.liked.all().count()
--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -41,11 +41,11 @@ const getData = () => {
         </div>
         <div class="card-footer">
         <div class="row">
-        <div class="col-1">
+        <div class="col-2">
             <a href="#" class="btn btn-primary">Details</a>
         </div>
         <div class="col-2">
-            <a href="#" class="btn btn-primary">Likes</a>
+            <a href="#" class="btn btn-primary">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</a>
         </div>
       </div>
     </div>
