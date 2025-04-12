7c1f524a0ea296e9e82cb2de232c1d5c97d8407a
soham-goswami <goswamisoham7202@gmail.com>
2025-04-11 21:02:55 -0400

    functionality added for load more post

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,54 +1,76 @@
 console.log('hello world')
 
-const helloWorldBox = document.getElementById('hello-world')
+// const helloWorldBox = document.getElementById('hello-world')
 const postsBox= document.getElementById('posts-box')
 const spinnerBox = document.getElementById('spinner-box')
+const loadbtn = document.getElementById('load-btn')
+const endBox = document.getElementById('end-box')
 // helloWorldBox.innerHTML =  'hello <b>world</b>'
 
-$.ajax({
-    type: 'GET',
-    url: '/hello-world/',
-    success: function(response){
-        console.log('success', response.text)
-        helloWorldBox.textContent = response.text
-    },
-    error: function(error){
-        console.log('error', error)
-    }
+// $.ajax({
+//     type: 'GET',
+//     url: '/hello-world/',
+//     success: function(response){
+//         console.log('success', response.text)
+//         helloWorldBox.textContent = response.text
+//     },
+//     error: function(error){
+//         console.log('error', error)
+//     }
+// })
+
+let visible = 3
+
+const getData = () => {
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}/`,
+        success: function(response){
+            console.log(response)
+            const data = response.data
+            setTimeout(()=>{
+                spinnerBox.classList.add('not-visible')
+                console.log(data)
+                data.forEach(el => {
+                    postsBox.innerHTML += `
+                     <div class="card mb-2">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${el.title}</h5>
+        <p class="card-text">${el.body}</p>
+        </div>
+        <div class="card-footer">
+        <div class="row">
+        <div class="col-1">
+            <a href="#" class="btn btn-primary">Details</a>
+        </div>
+        <div class="col-2">
+            <a href="#" class="btn btn-primary">Likes</a>
+        </div>
+      </div>
+    </div>
+                    `
+                });
+            }, 1000)
+            console.log(response.size)
+            if (response.size === 0) {
+                endBox.textContent = 'No posts added yet..'
+            }
+            else if (response.size <= visible) {
+                loadbtn.classList.add('not-visible')
+                endBox.textContent = 'No more pots to load..'
+            }
+        },
+        error: function(error){
+            console.log(error)
+        }
+    })   
+}
+
+loadbtn.addEventListener('click', ()=>{
+    spinnerBox.classList.remove('not-visible')
+    visible +=3
+    getData()
 })
 
-$.ajax({
-    type: 'GET',
-    url: '/data/',
-    success: function(response){
-        console.log(response)
-        const data = response.data
-        setTimeout(()=>{
-            spinnerBox.classList.add('not-visible')
-            console.log(data)
-            data.forEach(el => {
-                postsBox.innerHTML += `
-                 <div class="card mb-2">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${el.title}</h5>
-    <p class="card-text">${el.body}</p>
-    </div>
-    <div class="card-footer">
-    <div class="row">
-    <div class="col-1">
-        <a href="#" class="btn btn-primary">Details</a>
-    </div>
-    <div class="col-2">
-        <a href="#" class="btn btn-primary">Likes</a>
-    </div>
-  </div>
-</div>
-                `
-            });
-        }, 1000)
-    },
-    error: function(error){
-        console.log(error)
-    }
-})
+getData()
--- src/posts/views.py
+++ src/posts/views.py
@@ -18,7 +18,12 @@ def post_list_and_create(request):
     qs = Post.objects.all()
     return render(request, 'posts/main.html', {'qs':qs})
 
-def load_post_data_view(request):
+def load_post_data_view(request, num_posts):  
+    visible = 3
+    upper = num_posts # 9
+    lower = upper - visible # 6
+    size = Post.objects.all().count()
+
     qs = Post.objects.all()
     data = []
     for obj in qs:
@@ -26,10 +31,11 @@ def load_post_data_view(request):
             'id': obj.id,
             'title': obj.title,
             'body': obj.body,
+            'liked': True if request.user in obj.liked.all() else False,
             'author': obj.author.user.username
         }
         data.append(item)
-    return JsonResponse({'data':data})
+    return JsonResponse({'data':data[lower:upper], 'size': size})
 
 def hello_world_view(request):
     return JsonResponse({'text': 'hello world x2'})
